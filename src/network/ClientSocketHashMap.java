38c991decd91cc940d5b4d2ea7804ed8034ad270
Iason Andriopoulos <iason.andriopoulos@gmail.com>
2015-11-11 08:03:11 +0200

    Some additional functionality for controls in clients hash map

--- src/network/ClientSocketHashMap.java
+++ src/network/ClientSocketHashMap.java
@@ -4,18 +4,28 @@ import java.util.HashMap;
 
 public class ClientSocketHashMap {
 	private HashMap<String, JClientSocketManager> connections;
+	private int maximumSize;
 
-	public ClientSocketHashMap() {
+	public ClientSocketHashMap(int max) {
 		this.connections = new HashMap<String, JClientSocketManager>();
+		this.maximumSize = max;
 	}
 
 	public void addConnection(JClientSocketManager manager) {
 		this.connections.put(manager.getUID(), manager);
 	}
 
+	public boolean containsConnection(String uid) {
+		return connections.containsKey(uid);
+	}
+
 	public JClientSocketManager getConnection(String uid)
 			throws NullPointerException {
 		return this.connections.get(uid);
+	}
+
+	public boolean isFull() {
+		return connections.size() >= maximumSize;
 	}
 
 	public void removeConnection(String uid) {
