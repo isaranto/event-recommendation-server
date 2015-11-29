bb58f5dc67e5f561d0ab08dcbbee341c7d03a934
Iason Andriopoulos <iason.andriopoulos@gmail.com>
2015-11-29 12:51:30 +0200

    [DEV] Importing of project variables from json file

---
+++ src/util/ProjectVariables.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.io.FileReader;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class ProjectVariables {
+
+	/*
+	 * Loading project parameters from an external Json file. Please view the
+	 * example Json file on the project's directory for more details.
+	 */
+
+	private static String filepath = "project_vars.json";
+	private static JSONObject projectVars;
+
+	public static Object getValue(String key) {
+		return projectVars.get(key);
+	}
+
+	public static void loadProjectVars() throws Exception {
+		projectVars = (JSONObject) new JSONParser().parse(new FileReader(
+				filepath));
+	}
+
+}
