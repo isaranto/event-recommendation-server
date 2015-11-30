d4ea58893562b353784aceca882be872ad976065
Iason Andriopoulos <iason.andriopoulos@gmail.com>
2015-11-30 17:55:21 +0200

    [FIX] Cutting off bot connections
    -Very frequent requests are made from bot clients
    and not actual clients so we reject them.
    -Room from implementation: use a bot blacklist to
    prevent reconnection attempts

--- src/network/ClientSocketSlave.java
+++ src/network/ClientSocketSlave.java
@@ -3,6 +3,8 @@ package network;
 import java.io.DataInputStream;
 import java.io.IOException;
 
+import util.ProjectVariables;
+
 public class ClientSocketSlave extends Thread {
 
 	private DataInputStream in;
@@ -14,10 +16,19 @@ public class ClientSocketSlave extends Thread {
 	}
 
 	public void listen() {
+		long last_timestamp = 0;
 		while (true) {
 			try {
-				String message = in.readUTF();
-				clientManager.deliverMessage(message);
+				long time_in_seconds = System.nanoTime() / 1000000000;
+				long difference = time_in_seconds - last_timestamp;
+				if (difference > ProjectVariables
+						.getIntValue("message_time_gamp")) {
+					last_timestamp = time_in_seconds;
+					String message = in.readUTF();
+					clientManager.deliverMessage(message);
+				} else {
+					throw new IOException();
+				}
 			} catch (IOException io) {
 				clientManager.deliverIOException(io);
 				break;
--- src/tasks/JTaskManager.java
+++ src/tasks/JTaskManager.java
@@ -6,11 +6,6 @@ public class JTaskManager implements TaskManager {
 
 	@Override
 	public void handleMessageFromNetwork(String msg, String uID) {
-		try {
-			Thread.sleep(2000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 		sendMessageToNetwork("Hi friend", uID);
 	}
 
